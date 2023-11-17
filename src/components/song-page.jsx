22d1f13fa69ba2af2559c72b3eb6be27eea37295
unknown <davgio7@gmail.com>
2023-11-17 13:27:50 +0000

    added all to the server

--- src/components/song-page.jsx
+++ src/components/song-page.jsx
@@ -10,12 +10,10 @@ import { AuthContext } from "../components/context/AuthContext";
 import "../css/songpage.css";
 
 const SongPage = (props) => {
-    console.log(props)
     const auth = useContext(AuthContext);
 
     let navigate = useNavigate();
     let { state } = useLocation();
-    console.log(state)
     const [lyric, setLyric] = useState("");
     const [copyRight, setCopyright] = useState("null");
     const [artist, setArtist] = useState("");
@@ -53,53 +51,64 @@ const SongPage = (props) => {
             return;
         }
 
-        Promise.all([
-            fetch(
-                `/ws/1.1/track.lyrics.get?track_id=${trackId}&apikey=${process.env.VITE_API_KEY_MUSICMATCH}`
-            ),
-            fetch(
-                `/ws/1.1/track.search?q_track=${songTrack}&apikey=${process.env.VITE_API_KEY_MUSICMATCH}`
-            ),
-            fetch(
-                `/ws/1.1/album.tracks.get?album_id=${idAlbum}&apikey=${process.env.VITE_API_KEY_MUSICMATCH}`
-            ),
-            fetch(
-                `/cover/2.0/?method=album.search&album=${album}&api_key=${process.env.VITE_API_KEY_LASTFM}&format=json`,
-                { signal }
-            ),
-        ])
-            .then((res) => Promise.all(res.map((res) => res.json())))
-            .then((data) => {
-                const lyrics = data[0].message.body.lyrics;
-                const songTitle = data[1].message.body.track_list;
-                const albumTracksList = data[2].message.body.track_list;
-                const coverAlbum = data[3].results.albummatches.album[0];
-                if (typeof lyrics !== "undefined") {
-                    setLyric(lyrics.lyrics_body);
-                    setCopyright(lyrics.lyrics_copyright);
-                } else {
-                    return;
-                }
-                setSongTitle(songTitle[0].track.track_name);
-                setAlbumId(albumTracksList);
-                if (typeof coverAlbum !== "undefined") {
-                    setCover(coverAlbum.image[3]["#text"]);
-                    setArtist(coverAlbum.artist);
 
-                    setAlbumTitle(coverAlbum.name);
-                } else {
-                    setCover(defImage);
-                }
-            })
-            .catch((err) => console.log(err));
+        const songFecth = async () => {
+            const response = await fetch(
+                `/v.1/api/songs/${trackId}/${songTrack}/${idAlbum}/${album}`, { signal }
+            )
 
-        return function cleanUp() {
-            abortControlledApi.abort();
+            const songsData = await response.json();
+            const lyrics = songsData.data[0].message.body.lyrics;
+            const songTitle = songsData.data[1].message.body.track_list;
+            const albumTracksList = songsData.data[2].message.body.track_list;
+            const coverAlbum = songsData.data[3].results.albummatches.album[0];
+            if (typeof lyrics !== "undefined") {
+                setLyric(lyrics.lyrics_body);
+                setCopyright(lyrics.lyrics_copyright);
+            } else {
+                return;
+            }
+            setSongTitle(songTitle[0].track.track_name);
+            setAlbumId(albumTracksList);
+            if (typeof coverAlbum !== "undefined") {
+                setCover(coverAlbum.image[3]["#text"]);
+                setArtist(coverAlbum.artist);
+                setAlbumTitle(coverAlbum.name);
+            } else {
+                setCover(defImage);
+            }
+
+            // const lyrics = songsData.lyrics.lyrics_body;
+            // const songTitle = songsData.songTitle[1].track.track_name;
+            // const albumTracksList = songsData.albumTracksList[0].track.track_name
+            // console.log(';;;;;;;;;', albumTracksList)
+            // const coverAlbum = songsData.coverAlbum;
+            // if (typeof lyrics !== "undefined") {
+            //     setLyric(lyrics);
+            //     setCopyright(lyrics.lyrics_copyright);
+            // } else {
+            //     return;
+            // }
+            // setSongTitle(songTitle);
+            // setAlbumId(albumTracksList);
+            // if (typeof coverAlbum !== "undefined") {
+            //     setCover(coverAlbum.image[3]["#text"]);
+            //     setArtist(coverAlbum.artist);
+            //     setAlbumTitle(coverAlbum.name);
+            // } else {
+            //     setCover(defImage);
+            // }
+            // console.log(coverAlbum)
         };
-    }, [state]);
+        songFecth()
+    }, [state])
 
-    const getAlbumTracks = (idTrack, idAlbum, ...props) => {
-        let prevData = props.map((item) => {
+
+
+
+    const getAlbumTracks = (idTrack, idAlbum, ...state) => {
+        console.log(state)
+        let prevData = state.map((item) => {
             return {
                 album_id: item.album_id,
                 album_name: item.album_name,
@@ -111,84 +120,84 @@ const SongPage = (props) => {
         });
         setUpdateState(...prevData);
 
-        Promise.all([
-            fetch(
-                `/ws/1.1/track.lyrics.get?track_id=${idTrack}&apikey=${process.env.VITE_API_KEY_MUSICMATCH}`
-            ),
-            fetch(
-                `/ws/1.1/album.tracks.get?album_id=${idAlbum}&apikey=${process.env.VITE_API_KEY_MUSICMATCH}`
-            ),
-        ])
-            .then((res) => Promise.all(res.map((res) => res.json())))
-            .then((data) => {
-                console.log(data);
-                const lyric = data[0].message.body.lyrics;
-                console.log(lyric);
-                setLyric(lyric.lyrics_body);
+        const fetchAlbumTracks = async () => {
+            const response = await fetch(
+                `/v.1/api/albumTrack/${idTrack}/${idAlbum}`
+            )
+            const data = await response.json();
+            console.log('data: ', data)
+            const lyric = data.data[0].message.body.lyrics;
+            console.log(lyric);
+            setLyric(lyric.lyrics_body);
 
-                const songName = data[1].message.body.track_list;
-                songName &&
-                    songName.map((item) => {
-                        return idTrack === item.track.track_id
-                            ? setSongTitle(item.track.track_name)
-                            : null;
-                    });
-            })
-            .catch((error) => console.log(error));
+            const songName = data.data[1].message.body.track_list;
+            songName &&
+                songName.map((item) => {
+                    return idTrack === item.track.track_id
+                        ? setSongTitle(item.track.track_name)
+                        : null;
+                });
+
+            //         .catch((error) => console.log(error));
+
+        }
+        fetchAlbumTracks()
     };
+
 
     const saveSong = async () => {
         setIsLoading(true);
         if (auth.isAuthenticated()) {
-        const dataToSave = {
-            ...updateState,
-            words: lyric,
-            userEmail: auth.authState.userInfo.email,
-        };
-        await fetch(`/v.1/api/song`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(dataToSave),
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data._id);
-                console.log(data.type)
-                setTimeout(() => {
-                    setIsLoading(false);
-                }, 400);
-                switch (data.type) {
-                    case "SUCCESS":
-                        return dispatch({
-                            type: "ADD_NOTIFICATION",
-                            payload: {
-                                id: data.id,
-                                type: data.type,
-                                title: "Success",
-                                message: data.message,
-                                icon: String.fromCharCode(10004),
-                            },
-                        });
-                    case "EXIST":
-                        return dispatch({
-                            type: "ADD_NOTIFICATION",
-                            payload: {
-                                id: data.id,
-                                type: data.type,
-                                title: "Exist",
-                                message: data.message,
-                                icon: String.fromCharCode(9940),
-                            },
-                        });
-                    default:
-                        return;
-                }
+            const dataToSave = {
+                ...updateState,
+                words: lyric,
+                userEmail: auth.authState.userInfo.email,
+            };
+            console.log(dataToSave)
+            await fetch(`/v.1/api/song`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(dataToSave),
             })
-            .catch((error) => {
-                console.error(error);
-            });
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                    console.log(data.type)
+                    setTimeout(() => {
+                        setIsLoading(false);
+                    }, 400);
+                    switch (data.type) {
+                        case "SUCCESS":
+                            return dispatch({
+                                type: "ADD_NOTIFICATION",
+                                payload: {
+                                    id: data.id,
+                                    type: data.type,
+                                    title: "Success",
+                                    message: data.message,
+                                    icon: String.fromCharCode(10004),
+                                },
+                            });
+                        case "EXIST":
+                            return dispatch({
+                                type: "ADD_NOTIFICATION",
+                                payload: {
+                                    id: data.id,
+                                    type: data.type,
+                                    title: "Exist",
+                                    message: data.message,
+                                    icon: String.fromCharCode(9940),
+                                },
+                            });
+                        default:
+                            return;
+                    }
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
         } else {
             navigate("/signup");
         }
