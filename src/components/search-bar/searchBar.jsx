d3a34689c1f0a8ad5c755a8ced25ec7ade734a5e
unknown <davgio7@gmail.com>
2024-02-23 16:18:18 +0000

    added load more button functionality

--- src/components/search-bar/searchBar.jsx
+++ src/components/search-bar/searchBar.jsx
@@ -5,47 +5,52 @@ import Input from "../reusable/input";
 import Button from "../reusable/buttons/button";
 import Loader from "../reusable/loader";
 import ArtistCard from "../artist-card/artist-card";
-import '../../css/searchbar.css'
+import "../../css/searchbar.css";
 
 const SearchBar = () => {
   const [selectParam, setSelectParam] = useState("q_artist");
   const [paramToSearch, setParamToSerach] = useState("");
-  const [tune, setTune] = useState();
+  const [tune, setTune] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [displayedItems, setDisplayedItems] = useState(4); // Initial number of items to display
 
   let apiUrl;
 
-  if (import.meta.env.MODE === 'test') {
+  if (import.meta.env.MODE === "test") {
     apiUrl = process.env.VITE_API_TEST_URL;
-  } else if (import.meta.env.MODE === 'development') {
+  } else if (import.meta.env.MODE === "development") {
     apiUrl = process.env.VITE_API_URL;
   } else {
-    apiUrl = process.env.VITE_API_KEY_DOMAIN
+    apiUrl = process.env.VITE_API_KEY_DOMAIN;
   }
 
-
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const getData = async () => {
     const url = `${process.env.VITE_API_URL}/${selectParam}/${paramToSearch}`;
     setIsLoading(true);
 
-
-    const getData = async () => {
+    try {
       const res = await fetch(url, {
         method: "GET",
         headers: { "Content-type": "application/json" },
       });
 
       const data = await res.json();
-      console.log('data: ', data)
+      console.log("data: ", data);
       let song = data.map((item) => item.track);
-      setTune(song);
+      setTune((prevTune) => [...prevTune, ...song]);
       setIsLoading(false);
       setParamToSerach("");
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setIsLoading(false);
     }
-    getData();
+  };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setTune([]);
+    setDisplayedItems(4);
+    getData();
   };
 
   const handleChange = (e) => {
@@ -62,13 +67,22 @@ const SearchBar = () => {
       setSelectParam(newValue);
     }
   };
+
+  const loadMore = () => {
+    setDisplayedItems((prevItems) => prevItems + 4);
+  };
+
   return (
-    <div data-testid='searchBar' className="search-bar">
+    <div data-testid="searchBar" className="search-bar">
       <Header />
       <div className="field">
         <form className="form-u" onSubmit={handleSubmit}>
-          <label htmlFor='select Param'>Search a Song </label>
-          <select value={selectParam} onChange={getSelectionQuery} id="select Param">
+          <label htmlFor="select Param">Search a Song </label>
+          <select
+            value={selectParam}
+            onChange={getSelectionQuery}
+            id="select Param"
+          >
             <option value="q_artist">By Artist</option>
             <option value="q_track">By Song</option>
             <option value="q_lyrics">By Word</option>
@@ -90,20 +104,22 @@ const SearchBar = () => {
         {isLoading ? (
           <Loader />
         ) : (
-          tune &&
-          tune.map((song) => {
-            return (
+          tune
+            .slice(0, displayedItems)
+            .map((song) => (
               <ArtistCard
                 key={song.track_id}
                 track={song}
                 album={song.album_name}
               />
-            );
-          })
+            ))
         )}
       </div>
+      {tune.length > displayedItems && (
+        <div style={{width: '240px', margin: '0 auto'}}><Button onClick={() => loadMore()}>Load More</Button></div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
